54500c0c48266eea3ebbb4cf1ef3f852113bbd7d
Damian Helme <me@damianhelme.com>
2011-11-15 16:46:15 +0000

    added test to show difference between closures around requstVar and regular objects

---
+++ src/main/scala/code/snippet/OnSubmit.scala
@@ -0,0 +1,11 @@
+package code.snippet
+package code
+package snippet
+import net.liftweb._
+import http._
+import util.Helpers._
+import scala.xml.NodeSeq
+/**
+ * A snippet that binds behavior, functions,
+ * to HTML elements
+ */
